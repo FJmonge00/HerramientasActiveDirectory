96088cf3c47ac6f167b411c4f8620d7ef7359a06
FJmonge00 <55493893+FJmonge00@users.noreply.github.com>
2021-01-27 10:45:19 +0100

    pruebas
    pruebas

---
+++ script.ps1
@@ -0,0 +1,13 @@
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "megainfo211b.com" `
+-DomainNetbiosName "MEGAINFO211b" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
